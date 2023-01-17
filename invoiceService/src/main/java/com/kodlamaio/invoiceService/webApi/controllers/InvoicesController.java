88b08917388062539c6ad359a1996727cdacc5f1
Muhammed İnan <minan.65@hotmail.com>
2023-01-17 18:58:01 +0300

    create webApi

---
+++ invoiceService/src/main/java/com/kodlamaio/invoiceService/webApi/controllers/InvoicesController.java
@@ -0,0 +1,32 @@
+package com.kodlamaio.invoiceService.webApi.controllers;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kodlamaio.common.utilities.results.DataResult;
+import com.kodlamaio.invoiceService.business.abstracts.InvoiceService;
+import com.kodlamaio.invoiceService.business.responses.get.GetAllInvoiceResponse;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/invoices")
+@AllArgsConstructor
+public class InvoicesController {
+
+private InvoiceService invoiceService;
+	
+	@GetMapping("getall")
+	public ResponseEntity<?> getAll(){
+		DataResult<List<GetAllInvoiceResponse>> result = invoiceService.getAll();
+		if (result.isSuccess()) {
+			return ResponseEntity.ok(result);
+		}
+		
+		return ResponseEntity.badRequest().body(result);
+	}
+}
