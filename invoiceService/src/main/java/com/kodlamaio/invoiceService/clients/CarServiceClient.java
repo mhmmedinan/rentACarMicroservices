26932864658cd68138d4a394471498327100e9de
Muhammed İnan <minan.65@hotmail.com>
2023-01-17 18:23:53 +0300

    getallcarresponse method

--- invoiceService/src/main/java/com/kodlamaio/invoiceService/clients/CarServiceClient.java
+++ invoiceService/src/main/java/com/kodlamaio/invoiceService/clients/CarServiceClient.java
@@ -1,5 +1,14 @@
 package com.kodlamaio.invoiceService.clients;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.kodlamaio.invoiceService.business.responses.get.GetAllCarsResponse;
+
+@FeignClient(name="inventory-service")
 public interface CarServiceClient {
 
+	@GetMapping("/api/cars/getById/{carId}")
+	GetAllCarsResponse getByCarId(@PathVariable String carId);
 }
