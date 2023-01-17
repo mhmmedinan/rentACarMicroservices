be1e38b5e75053fb6c0822209adf2bb950d18712
Muhammed İnan <67782652+mhmmedinan@users.noreply.github.com>
2023-01-17 19:58:00 +0300

    Update InvertoryServiceClient.java

--- rentalService/src/main/java/com/kodlamaiorentalService/invertoryServiceClient/InvertoryServiceClient.java
+++ rentalService/src/main/java/com/kodlamaiorentalService/invertoryServiceClient/InvertoryServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.kodlamaiorentalService.business.responses.rentals.GetAllCarsResponse;
 
-@FeignClient(name ="localhost:9011",path = "/api/cars")
+@FeignClient(name ="localhost:9011/invertory-service",path = "/api/cars")
 public interface InvertoryServiceClient {
 
 	@GetMapping("/getById/{carId}")
