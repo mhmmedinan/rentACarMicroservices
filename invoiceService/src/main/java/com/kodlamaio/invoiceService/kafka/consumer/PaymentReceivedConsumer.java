cdd86f23caf8cebe9511676f37db4ca26bc95994
Muhammed İnan <minan.65@hotmail.com>
2023-01-17 18:39:36 +0300

    create paymentreceivedconsumer

--- invoiceService/src/main/java/com/kodlamaio/invoiceService/kafka/consumer/PaymentReceivedConsumer.java
+++ invoiceService/src/main/java/com/kodlamaio/invoiceService/kafka/consumer/PaymentReceivedConsumer.java
@@ -1,5 +1,39 @@
 package com.kodlamaio.invoiceService.kafka.consumer;
 
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.kodlamaio.common.events.rentals.PaymentReceivedEvent;
+import com.kodlamaio.common.utilities.mapping.ModelMapperService;
+import com.kodlamaio.invoiceService.business.abstracts.InvoiceService;
+import com.kodlamaio.invoiceService.business.responses.get.GetAllCarsResponse;
+import com.kodlamaio.invoiceService.clients.CarServiceClient;
+import com.kodlamaio.invoiceService.entities.Invoice;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
 public class PaymentReceivedConsumer {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(PaymentReceivedConsumer.class);
+	private ModelMapperService modelMapperService;
+	private CarServiceClient carServiceClient;
+	private InvoiceService invoiceService;
+
+	@KafkaListener(topics = "received_payment", groupId = "payment_received")
+	public void consume(PaymentReceivedEvent event) {
+		Invoice invoice = modelMapperService.forRequest().map(event, Invoice.class);
+		GetAllCarsResponse response = carServiceClient.getByCarId(event.getCarId());
+		invoice.setDailyPrice(event.getDailyPrice());
+		invoice.setTotalPrice(event.getTotalPrice());
+		invoice.setBrandName(response.getModelBrandName());
+		invoice.setModelName(response.getModelName());
+		invoice.setModelYear(response.getModelYear());
+		invoiceService.createInvoice(invoice);
+		LOGGER.info("Invoice created consume : {}", event.toString());
+	}
 
 }
